7f5778b8acd90a4adbbc533ff1f1cda385aa0c9a
José Ángel Álvarez <joseangelalvarezbaz13@gmail.com>
2025-01-21 21:17:01 +0100

    Added a funtion for future testing and improve options and settings management.

--- src/models/utils.py
+++ src/models/utils.py
@@ -6,6 +6,7 @@ class Json_controls:
     
     @staticmethod
     def get_anime_emi_data():
+
         try:
             with open('\\\\Casa-JJ\\Estudios\\PROYECTOS\\anime-track\\src\\data\\anime-emission.json', 'r') as emission:
                 dic_emi = json.load(emission)
@@ -16,6 +17,7 @@ class Json_controls:
 
     @staticmethod
     def save_anime_emi_data(dic_emi):
+
         try:
             with open('\\\\Casa-JJ\\Estudios\\PROYECTOS\\anime-track\\src\\data\\anime-emission.json', 'w') as emission:
                 json.dump(dic_emi,emission, indent=4)
@@ -23,8 +25,20 @@ class Json_controls:
             with open('src\\data\\anime-emission.json', 'w') as emission:
                 json.dump(dic_emi,emission, indent=4)
 
+    @staticmethod
+    def local_save():
+
+        try:
+            with open('\\\\Casa-JJ\\Estudios\\PROYECTOS\\anime-track\\src\\data\\config.json', 'r') as config_config:
+                config = json.load(config_config)
+        except:
+            with open('src\\data\\config.json', 'w') as config_config:
+                config = json.load(config_config)
+
+
 class Misc:
 
     @staticmethod
     def clear_window():
+
         os.system('cls' if os.name == "nt" else "clear")
